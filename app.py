a840790a439a7b1b1c4699966b8aff984279c49c
ranjitmane3525 <ranjit.mane3525@gmail.com>
2022-10-07 22:47:00 +0530

    path change

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ def predict():
     
     print(u_data)  
 
-    with open(r'D:\Python\Python Programming\My practice\Project\Salary Prediction\artifacts\model_lr.pkl','rb') as file:
+    with open(r'artifacts\model_lr.pkl','rb') as file:
          model1 = pickle.load(file)
          result1 =model1.predict([[u_data]])
          print(result1)
