e54ebbd34d1846971ce3a4e266fdd37c86071358
ranjitmane3525 <ranjit.mane3525@gmail.com>
2022-10-10 00:35:11 +0530

    debug added in app.py

--- app.py
+++ app.py
@@ -36,5 +36,5 @@ def predict():
     return render_template('index.html',nm =name1,result=var)
 
 if __name__ == '__main__':
-    app.run(host= "0.0.0.0",port=8080)
+    app.run(host= "0.0.0.0",port=8080,debug=True)
 
